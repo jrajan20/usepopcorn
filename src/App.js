18cbe289822f6dbb0f3db47becd16d5756e1d7b1
jrajan20 <jai_20j@hotmail.com>
2026-06-19 14:36:42 -0400

    Creating Star rating component

--- src/App.js
+++ src/App.js
@@ -264,30 +264,5 @@ function Box({element}){
   )
 
 }
-// function WatchedBox({children}){
-    
- 
-//  const [isOpen2, setIsOpen2] = useState(true);
-
-
-   
-
-//   return(
-//      <div className="box">
-//           <button
-//             className="btn-toggle"
-//             onClick={() => setIsOpen2((open) => !open)}
-//           >
-//             {isOpen2 ? "–" : "+"}
-//           </button>
-//           {isOpen2 && (
-//             <>
-//              {children}
-//             </>
-//           )}
-//         </div>
-//   )
-  
-// }
 
 
